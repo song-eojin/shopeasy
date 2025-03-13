9b8f2a3d9783daf7d4217b38d1310c494d75ea55
song eojin <feshed528@gmail.com>
2025-03-13 23:21:45 +0900

    init: 스프링부트 프로젝트 초기 환경설정

---
+++ src/test/java/com/personal/shopeasy/ShopeasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.shopeasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopeasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
